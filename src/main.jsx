91f8739717d7384d0bb85d8ddcd6e63178a67364
Lucas Valdez <lnvaldez24@gmail.com>
2025-02-02 09:51:38 -0300

    Feat: Add league logos

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Title from "./components/atoms/Title";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Title text="Football Wordle" />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
