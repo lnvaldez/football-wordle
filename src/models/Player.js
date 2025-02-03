853a0c27e1f1edd684972f8ab960e24748a028b3
Lucas Valdez <lnvaldez24@gmail.com>
2025-02-03 10:01:07 -0300

    Feat: Player model

---
+++ src/models/Player.js
@@ -0,0 +1,25 @@
+export class Player {
+  constructor({
+    thumb = "Unknown",
+    name = "Unknown",
+    league = "Unknown",
+    club = "Unknown",
+    age = "Unknown",
+    nationality = "Unknown",
+    number = "Unknown",
+    foot = "Unknown",
+    position = "Unknown",
+    height = "Unknown",
+  }) {
+    this.thumb = thumb;
+    this.name = name;
+    this.league = league;
+    this.club = club;
+    this.age = age;
+    this.nationality = nationality;
+    this.number = number;
+    this.foot = foot;
+    this.position = position;
+    this.height = height;
+  }
+}
